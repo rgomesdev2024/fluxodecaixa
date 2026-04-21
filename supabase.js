9aade67fae9ca235efd335a6088ea8a71f99f8b0
rgomesdev2024 <rodrigoprofissional2014@gmail.com>
2026-04-21 02:38:07 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "https://ryzunocfrrlvrtfnrxwi.supabase.co"
+const supabaseUrl = "https://rgomesdev2024.github.io/fluxodecaixa"
 
 const supabaseKey = "sb_secret_srfsjzS1oCGEgJwQB3RNdg_Z-sw3Kua"
 
